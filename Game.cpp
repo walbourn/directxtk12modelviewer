616c534a5ebb4a475103736653a58fc67896438e
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-06-15 11:45:53 -0700

    Updated for DirectXTK June 15, 2020 release

--- Game.cpp
+++ Game.cpp
@@ -885,8 +885,6 @@ void Game::CreateDeviceDependentResources()
     m_graphicsMemory = std::make_unique<GraphicsMemory>(device);
 
     m_resourceDescriptors = std::make_unique<DescriptorPile>(device,
-        D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
-        D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
         Descriptors::Count,
         Descriptors::Reserve);
 
